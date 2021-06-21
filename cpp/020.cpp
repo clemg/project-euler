f3e970fdbba633d20f15fa4013c1cd6b46944e02
Clément Guibout <clement.guibout@gmail.com>
2021-06-21 22:02:15 +0200

    020
    cpp

---
+++ cpp/020.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int,int> pi;
+typedef vector<vector<int>> tab2di;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define PI acos(-1)
+#define FOR(i,a,b) for (int i=a; i<b; i++)
+#define CFOR(i,a,b,c) for (int i=a; i<b; i+=c)
+#define RFOR(i,a,b) for (int i=a; i>=b; i--)
+#define RFORC(i,a,b,c) for (int i=a; i>=b; i-=c)
+#define FORA(i,v) for (auto i: v)
+
+
+int main() {
+	// calculated with python: print(__import__('math').factorial(100))
+	const string fact100 = "93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000";
+	int sum = 0;
+	
+	FOR(i,0,fact100.size()) sum+=fact100[i] - '0';
+	cout << sum;
+	
+
+	return 0;
+}
